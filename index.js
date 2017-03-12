c73d4053a9638d7911116b5dffefb62f910344f6
Martin Bing <bing.martin.photography@gmail.com>
2017-03-12 19:29:17 +0100

    Add static server and update files

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-/**
- * Created by martin.demiddel on 12/03/2017.
- */
+var static = require('node-static');
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./public/pattern-library/index.html');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
